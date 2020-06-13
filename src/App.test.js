5080b755782b3d93868a2c2bd0d58fbbcea07601
Kenzie Bottoms <kenziebottoms@gmail.com>
2020-06-13 13:59:27 -0500

    context works, general table renders text

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
+
 import App from './App'
+import Content from './Content'
+
+import { getCurrentInfo } from './CovidAPI'
+import { CovidContext } from './CovidContext'
 
 test('renders child components', () => {
   const { container } = render(<App />)
 
   expect(container.querySelector('#sidebar')).toBeInTheDocument()
   expect(container.querySelector('#content')).toBeInTheDocument()
+})
+
+test('renders general table', async () => {
+  let data = await getCurrentInfo()
+  const { getByText } = render(
+    <CovidContext.Provider value={{ data }}>
+      <Content />
+    </CovidContext.Provider>,
+  )
+
+  expect(getByText('Positive Tests')).toBeInTheDocument()
 })
