37a23a618c59d1441f1fe4557ca6faf190a4e5ce
Kenzie Bottoms <kenziebottoms@gmail.com>
2020-06-12 22:09:58 -0500

    show data for active state

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,32 +6,48 @@ export default function Content() {
   const { data, activeState } = useContext(CovidContext)
   return (
     <div id="content">
-      {activeState
-        ? JSON.stringify(activeState)
-        : data && (
-            <table>
-              <tbody>
-                <tr>
-                  <th></th>
-                  <th>Cases</th>
-                  <th>Deaths</th>
-                  <th>Positive Tests</th>
-                  <th>Negative Tests</th>
-                  <th>Total Tests</th>
+      {activeState ? (
+        <table class="activeState">
+          <tbody>
+            <tr>
+              <th colSpan="2">{activeState.state}</th>
+            </tr>
+            <tr>
+              <td>Data Quality</td>
+              <td>{activeState.dataQualityGrade}</td>
+            </tr>
+            <tr>
+              <td>Current Hospitalizations</td>
+              <td>{activeState.hospitalizedCurrently}</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        data && (
+          <table>
+            <tbody>
+              <tr>
+                <th></th>
+                <th>Cases</th>
+                <th>Deaths</th>
+                <th>Positive Tests</th>
+                <th>Negative Tests</th>
+                <th>Total Tests</th>
+              </tr>
+              {data.map((state) => (
+                <tr key={state.state} id={state.state}>
+                  <td>{state.state}</td>
+                  <td>{state.positiveCasesViral}</td>
+                  <td>{state.death}</td>
+                  <td>{state.positive}</td>
+                  <td>{state.negative}</td>
+                  <td>{state.totalTestsViral}</td>
                 </tr>
-                {data.map((state) => (
-                  <tr key={state.fips} id="{state.fips}">
-                    <td>{state.state}</td>
-                    <td>{state.positiveCasesViral}</td>
-                    <td>{state.death}</td>
-                    <td>{state.positive}</td>
-                    <td>{state.negative}</td>
-                    <td>{state.totalTestsViral}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
+              ))}
+            </tbody>
+          </table>
+        )
+      )}
     </div>
   )
 }
