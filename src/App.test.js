c93203e3084c1fe40bc1a23c80899b8af3c14aab
Kenzie Bottoms <kenziebottoms@gmail.com>
2020-06-13 14:02:29 -0500

    Sidebar render test

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,10 @@ import React from 'react'
 import { render } from '@testing-library/react'
 
 import App from './App'
+import Sidebar from './Sidebar'
 import Content from './Content'
 
-import { getCurrentInfo } from './CovidAPI'
+import { getCurrentInfo, getStateInfo } from './CovidAPI'
 import { CovidContext } from './CovidContext'
 
 test('renders child components', () => {
@@ -14,8 +15,22 @@ test('renders child components', () => {
   expect(container.querySelector('#content')).toBeInTheDocument()
 })
 
-test('renders general table', async () => {
+test('Sidebar renders list of states', async () => {
   let data = await getCurrentInfo()
+
+  const { getByText } = render(
+    <CovidContext.Provider value={{ data }}>
+      <Sidebar />
+    </CovidContext.Provider>,
+  )
+
+  expect(getByText('AK')).toBeInTheDocument()
+  expect(getByText('TN')).toBeInTheDocument()
+})
+
+test('Content renders general table', async () => {
+  let data = await getCurrentInfo()
+
   const { getByText } = render(
     <CovidContext.Provider value={{ data }}>
       <Content />
@@ -23,4 +38,18 @@ test('renders general table', async () => {
   )
 
   expect(getByText('Positive Tests')).toBeInTheDocument()
+  expect(getByText('Negative Tests')).toBeInTheDocument()
+})
+
+test('Content renders detail view', async () => {
+  let activeState = await getStateInfo('tn')
+
+  const { getByText } = render(
+    <CovidContext.Provider value={{ activeState }}>
+      <Content />
+    </CovidContext.Provider>,
+  )
+
+  expect(getByText('TN')).toBeInTheDocument()
+  expect(getByText('Data Quality')).toBeInTheDocument()
 })
