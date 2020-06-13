fd65db408b56038de13ab20c8438174553e9ebab
Kenzie Bottoms <kenziebottoms@gmail.com>
2020-06-12 19:44:26 -0500

    scss, add content and sidebar blank components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './img/logo.svg';
-import './css/App.css';
+import React from 'react'
+
+import './style.scss'
+
+import Sidebar from './components/Sidebar'
+import Content from './components/Content'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar />
+      <Content />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
